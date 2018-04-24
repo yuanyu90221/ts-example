22c199b678453cc490b5de99e528e3d879e86fa4
yuanyu <yuanyu90221@gmail.com>
2018-04-25 01:12:31 +0800

    modify interface in child interface

--- interfaces.js
+++ interfaces.js
@@ -9,4 +9,14 @@ function car_1(automobile) {
     automobile.speed = 500;
     console.log("this " + automobile.brand + " is going at " + automobile.speed + " miles an hour");
 }
-car_1(automobile);
+// car_1(automobile);
+var AutoMobileClass = /** @class */ (function () {
+    function AutoMobileClass() {
+    }
+    AutoMobileClass.prototype.speedMethod = function (speed) {
+        console.log("Hi my car is going at " + speed);
+    };
+    return AutoMobileClass;
+}());
+var carObj = new AutoMobileClass();
+carObj.speedMethod(1000);
--- interfaces.ts
+++ interfaces.ts
@@ -1,8 +1,16 @@
 interface AutomobileInterface {
+  brand?: string;
+  speed?: number;
+  speedMethod?(velocidad: number): void;
+}
+interface AutomobileInterface2 extends AutomobileInterface {
   brand: string;
   speed: number;
-  speedMethod(velocidad: number): void;
 }
+
+const automobile2: AutomobileInterface2 = {
+  brand: "Porsche",
+};
 const automobile: AutomobileInterface = {
   brand: "BMW",
   speed: 200,
@@ -16,4 +24,14 @@ function car_1(automobile: AutomobileInterface) {
   console.log(`this ${automobile.brand} is going at ${automobile.speed} miles an hour`);
 }
 
-car_1(automobile);
+// car_1(automobile);
+class AutoMobileClass implements AutomobileInterface {
+  brand: string;
+  speed: number;
+  speedMethod(speed) {
+    console.log(`Hi my car is going at ${speed}`);
+  }
+}
+
+let carObj = new AutoMobileClass();
+carObj.speedMethod(1000);
