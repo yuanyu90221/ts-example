cb0601afe4c50c6fb33a41c98155540b7c4c3c2f
yuanyu <yuanyu90221@gmail.com>
2018-05-02 02:50:19 +0800

    generic with constraints

--- generic_classes.ts
+++ generic_classes.ts
@@ -1,6 +1,6 @@
 let array1: number[] = [1,2323,3];
 let array2: Array<string> = ["12","23"];
-class objectGenericsClass<TYPE1, TYPE2>{
+class objectGenericsClass<TYPE1, TYPE2 extends string>{
   
   constructor(public value1: TYPE1, public value2: TYPE2) {
 
@@ -9,6 +9,6 @@ class objectGenericsClass<TYPE1, TYPE2>{
 
 }
 
-let object1 = new objectGenericsClass<string,number>("HELLO",2);
+let object1 = new objectGenericsClass<string, string>("HELLO","2");
 
-let object2 = new objectGenericsClass<boolean, number>(true,2);
+let object2 = new objectGenericsClass<boolean, string>(true,"2");
